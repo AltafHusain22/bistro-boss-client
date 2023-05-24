69fc743f3e3499f049430ce4f775b06d7ec9006f
AltafHusain22 <fiverr.contactme@gmail.com>
2023-05-24 20:08:20 +0600

    initail setup done and completed router setup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Router/routers.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import Main from "../Layout/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+	children: [
+		{
+			path: '/',
+			element: <Home></Home>
+		}
+	]
+  },
+]);
