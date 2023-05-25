56d80f8dcc951f22cf520a3c1f8ed830b11df8fe
AltafHusain22 <fiverr.contactme@gmail.com>
2023-05-25 14:41:16 +0600

    about section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import Banner from "./HomeComponents/Banner/Banner";
 import CategoryCarosel from "./HomeComponents/CategoryCarosel/CategoryCarosel";
+import About from "./HomeComponents/About/About";
 
 const Home = () => {
   return (
     <div>
         <Banner></Banner>
         <CategoryCarosel></CategoryCarosel>
+        <About></About>
     </div>
   );
 };
--- src/Pages/Home/HomeComponents/About/About.jsx
+++ src/Pages/Home/HomeComponents/About/About.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import "./About.css";
 
 const About = () => {
-	return (
-		<div>
-			
-		</div>
-	);
+  return (
+    <section className="my-10">
+      <div className="bg-image py-10">
+        <div className=" bg-base-100 w-2/3 mx-auto my-10">
+          <div className="card-body text-center">
+            <h2 className=" text-xl md:text-4xl mb-5">Bistro Boss</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit magnam commodi quos expedita ducimus harum numquam. Porro soluta, ipsa accusamus, at reiciendis enim aspernatur iusto similique doloremque mollitia, aliquid molestias sed sunt facilis magnam saepe perferendis blanditiis nihil libero corporis velit! Soluta earum quo accusamus odit temporibus labore! Culpa, quaerat.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
