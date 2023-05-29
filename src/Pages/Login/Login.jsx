2491b852db30756c28e2fbea6fcc4d0256b57b68
AltafHusain22 <fiverr.contactme@gmail.com>
2023-05-29 14:29:25 +0600

    login page updated

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unknown-property */
-
+import "./Login.css";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
@@ -11,12 +11,17 @@ import {
   validateCaptcha,
 } from "react-simple-captcha";
 import { useEffect } from "react";
+import { ImFacebook } from "react-icons/im";
+import { FcGoogle } from "react-icons/fc";
+import { BiFingerprint } from "react-icons/bi";
+import { MdAlternateEmail } from "react-icons/md";
+import { BsCheck } from "react-icons/bs";
 
 const Login = () => {
-  // handle captcha 
-  useEffect(()=>{
-    loadCaptchaEnginge(6); 
-  },[])
+  // handle captcha
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
 
   // handlegoogle signUp
   const handleGoogleSignUp = () => {};
@@ -49,18 +54,7 @@ const Login = () => {
               <ul className="grid grid-cols-1 mt-10 sm:grid-cols-2 gap-x-8 gap-y-4">
                 <li className="flex items-center space-x-3">
                   <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
-                    <svg
-                      className="w-3.5 h-3.5 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
+                    <BsCheck className="text-white"></BsCheck>
                   </div>
                   <span className="text-lg font-medium text-white">
                     Quality Foods
@@ -68,18 +62,7 @@ const Login = () => {
                 </li>
                 <li className="flex items-center space-x-3">
                   <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
-                    <svg
-                      className="w-3.5 h-3.5 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
+                    <BsCheck className="text-white"></BsCheck>
                   </div>
                   <span className="text-lg font-medium text-white">
                     Fast Delivery
@@ -87,18 +70,7 @@ const Login = () => {
                 </li>
                 <li className="flex items-center space-x-3">
                   <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
-                    <svg
-                      className="w-3.5 h-3.5 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
+                    <BsCheck className="text-white"></BsCheck>
                   </div>
                   <span className="text-lg font-medium text-white">
                     Fresh Foods
@@ -106,18 +78,7 @@ const Login = () => {
                 </li>
                 <li className="flex items-center space-x-3">
                   <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
-                    <svg
-                      className="w-3.5 h-3.5 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
+                    <BsCheck className="text-white"></BsCheck>
                   </div>
                   <span className="text-lg font-medium text-white">
                     Reasonable Price
@@ -152,101 +113,68 @@ const Login = () => {
                     Email address
                   </label>
                   <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                      <svg
-                        className="w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          ssstrokelinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                        />
-                      </svg>
+                    <div className="email-field-wrap">
+                      <MdAlternateEmail></MdAlternateEmail>
                     </div>
-
                     <input
                       type="email"
                       name="email"
                       placeholder="Enter email to get started"
-                      className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                      className="email-input-field"
                     />
                   </div>
                 </div>
                 {/* password */}
                 <div>
                   <div className="flex items-center justify-between">
-                    <label
-                      htmlFor=""
-                      className="text-base font-medium text-gray-900"
-                    >
+                    <label className="text-base font-medium text-gray-900">
                       Password
                     </label>
 
-                    <Link
-                      to={"#"}
-                      className="text-sm font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 focus:text-blue-700 hover:underline"
-                    >
+                    <Link to={"#"} className="forgot-link">
                       Forgot password?
                     </Link>
                   </div>
                   <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                      <svg
-                        className="w-5 h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          ssstrokelinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-                        />
-                      </svg>
+                    <div className="pass-field-wrap">
+                      <BiFingerprint></BiFingerprint>
                     </div>
 
                     <input
                       type="password"
                       name="password"
                       placeholder="Enter your password"
-                      className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                      className="pass-input-field"
                     />
                   </div>
                 </div>
                 {/* captcha field */}
                 <div>
                   <div className="flex items-center justify-between">
-                    <label
-                      htmlFor=""
-                      className="text-base font-medium text-gray-900"
-                    >
-                      <LoadCanvasTemplate/>
+                    <label className="text-base font-medium text-gray-900">
+                      <LoadCanvasTemplate />
                     </label>
                   </div>
-                  <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
+                  <div className="captcha-wrap">
                     <input
                       type="text"
                       name="captcha"
                       placeholder="Enter above captcha"
-                      className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                      className="captcha-input-field"
                     />
                   </div>
+                  <button className=" mt-5 w-full btn btn-outline btn-sm">
+                    Validate
+                  </button>
                 </div>
 
                 <div>
-                  <button
+                  <input
+                    disabled
                     type="submit"
-                    className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 border border-transparent rounded-md bg-gradient-to-r from-fuchsia-600 to-blue-600 focus:outline-none hover:opacity-80 focus:opacity-80"
-                  >
-                    Log in
-                  </button>
+                    className="login-btn"
+                    value="Login"
+                  />
                 </div>
               </div>
             </form>
@@ -255,34 +183,17 @@ const Login = () => {
               <button
                 onClick={handleGoogleSignUp}
                 type="button"
-                className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
+                className="sign-with-google-btn"
               >
                 <div className="absolute inset-y-0 left-0 p-4">
-                  <svg
-                    className="w-6 h-6 text-rose-500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                  >
-                    <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                  </svg>
+                  <FcGoogle></FcGoogle>
                 </div>
                 Sign in with Google
               </button>
 
-              <button
-                type="button"
-                className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
-              >
+              <button type="button" className="sign-with-fb-btn">
                 <div className="absolute inset-y-0 left-0 p-4">
-                  <svg
-                    className="w-6 h-6 text-[#2563EB]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                  >
-                    <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                  </svg>
+                  <ImFacebook className="text-blue-800"></ImFacebook>
                 </div>
                 Sign in with Facebook
               </button>
